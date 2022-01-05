1673e74b909741812cd55ee80369662f9f9049ab
EAghayev <eaghayevv@gmail.com>
2022-01-05 18:20:43 +0400

    project added

---
+++ PustokP201/PustokP201/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PustokP201.Areas.Manage.ViewModels;
+using PustokP201.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PustokP201.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AdminLoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser admin = await  _userManager.FindByNameAsync(loginVM.UserName);
+
+            if(admin == null)
+            {
+                ModelState.AddModelError("", "UserName or Passowrd is incorrect!");
+                return View();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(admin, loginVM.Password,false,false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "UserName or Passowrd is incorrect!");
+                return View();
+            }
+
+
+            return RedirectToAction("index", "dashboard");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("login", "account");
+        }
+    }
+}
